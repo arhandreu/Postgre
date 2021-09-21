7fe1e9322b364eaed3b9e4e37003714c9811eea2
Andreu Bardukov <arhandreu@gmail.com>
2021-09-21 13:07:58 +0500

    ДЗ_4

---
+++ HW4/DB_SiteMusic_Select.py
@@ -0,0 +1,13 @@
+import psycopg2
+import sqlalchemy
+
+db = 'postgresql://ab:5015@localhost:5432/musicsite'
+engine = sqlalchemy.create_engine(db)
+connection = engine.connect()
+
+print(list(connection.execute("""SELECT title, year FROM album WHERE year = 1977""")))
+print(list(connection.execute("""SELECT title, duration FROM track ORDER BY duration DESC LIMIT 1;""")))
+print(list(connection.execute("""SELECT title FROM track WHERE duration >= 210;""")))
+print(list(connection.execute("""SELECT title FROM collection WHERE year BETWEEN 2018 AND 2020;""")))
+print(list(connection.execute("""SELECT name FROM singer WHERE name NOT LIKE '%% %%';""")))
+print(list(connection.execute("""SELECT title FROM track WHERE title LIKE '%%мода%%';""")))
